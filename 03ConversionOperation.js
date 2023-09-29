36543e9dbe71c4963899aabba5fd2d5c529c6846
Shrikant204 <21021335@geu.ac.in>
2023-09-30 02:08:46 +0530

    Comparsion Operators

--- 03ConversionOperation.js
+++ 03ConversionOperation.js
@@ -25,3 +25,33 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3 
+let negValue = -value;
+console.log(negValue);
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Shrikant"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =2+3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
