8608aab8f5a9503b5f88bfde4f6c51c2592e7439
author <21021335@geu.ac.in>
2023-09-30 00:08:39 +0530

    variables and datatypes

---
+++ 01variables.js
@@ -0,0 +1,20 @@
+const accountId=112323
+let accountEmail="shri@gmail.com"
+var accountPass="1224"
+accountCity ="Jaipur"
+let accountstate
+
+//accountId=2
+
+accountEmail ="hitesh@gmail.com"
+accountPassword ="234234"
+accountCity="bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+
+/*
+Prefer not to use car 
+because of issue in block scope and function scope
+*/
---
+++ 02datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert (3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+console.log("shrikant")  
+//code readability should be high 
+
+let name="shrikant"
+let age = 18
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string =>" "
+// boolean =>true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof "shrikant") //string
+console.log(typeof null) //
