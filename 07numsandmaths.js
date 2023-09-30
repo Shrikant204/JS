8660d367b31af6cdc36bb358ef1055ea1daa330e
Shrikant204 <21021335@geu.ac.in>
2023-09-30 17:08:25 +0530

    numbers in JS

---
+++ 07numsandmaths.js
@@ -0,0 +1,33 @@
+const score = 400 
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 23.677
+
+//console.log(otherNumber.toPrecision(3));
+
+//const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// **************** Maths ******************* 
+
+//console.log(Math.random());
+//console.log(Math);
+//console.log(Math.abs(-123));
+//console.log(Math.floor(4.8));
+//console.log(Math.ceil(4.1));
+//console.log(Math.round(4.49));
+//console.log(Math.round(4.5));
+//console.log(Math.min(4,5,7687,6,867,1,-34232));
+
+
+console.log((Math.random()*10)+1);
+const min = 10
+const max = 20
+
+console.log((Math.random() * (max - min + 1) )
